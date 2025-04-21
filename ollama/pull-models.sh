e170707ea3944eb3105c36463174d9cbd5b18446
Dinith Kumudika <dinithwalpitagama@gmail.com>
2025-04-21 12:53:41 +0530

    models updated

--- ollama/pull-models.sh
+++ ollama/pull-models.sh
@@ -9,7 +9,7 @@ sleep 5
 # Pull llama 3 and nomic-embed
 ollama pull llama3.1:latest
 ollama pull nomic-embed-text:latest
-ollama pull deepseek-r1:7b
+ollama pull deepseek-r1:8b
 
 # Wait for the Ollama server to finish 
 wait $!
